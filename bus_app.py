f451c0e33103bb7b3b24a69c0ab2171a8e4d4559
villank2 <33633495+villank2@users.noreply.github.com>
2019-09-27 13:45:23 +0100

    Add files via upload

--- bus_app.py
+++ bus_app.py
@@ -6,6 +6,12 @@ def main():
     dbus = requests.get('https://www.dublinbus.ie/RTPI/Sources-of-Real-Time-Information/?',params=payload)
     soup = BeautifulSoup(dbus.content, 'html.parser')
     t = soup.find('table',{'id':'rtpi-results'})
-    
+    output = (t.find_all('td'))
+    string = ''
+    for out in output:
+        string += '-' + (str(out.string).strip()) 
+    li = (string.split('None'))
+    for real_time in li:
+        print(real_time)
 if __name__ == '__main__':
     main()
